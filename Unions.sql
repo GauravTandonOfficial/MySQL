6b2ad855a3a57da12e032cf3d8f1b51ad33ca318
GauravTandonOfficial <gauravtandon56@gmail.com>
2025-11-07 22:59:05 +0530

    Add files via upload

---
+++ Unions.sql
@@ -0,0 +1,36 @@
+SELECT age,gender 
+FROM parks_and_recreation.employee_demographics
+UNION
+SELECT first_name,last_name
+FROM parks_and_recreation.employee_salary;
+
+SELECT first_name,last_name
+FROM parks_and_recreation.employee_demographics
+UNION ALL
+SELECT first_name,last_name
+FROM parks_and_recreation.employee_salary;
+
+SELECT first_name,last_name,'Old' AS Label
+FROM parks_and_recreation.employee_demographics
+WHERE age>50;
+
+SELECT first_name,last_name,'Old' AS Label
+FROM parks_and_recreation.employee_demographics
+WHERE age>50
+UNION
+SELECT first_name,last_name,'Highly Paid Employee' AS Label
+FROM parks_and_recreation.employee_salary
+WHERE salary>75000;
+
+SELECT first_name,last_name,'Old Man' AS Label
+FROM parks_and_recreation.employee_demographics
+WHERE age>40 AND gender='Male'
+UNION
+SELECT first_name,last_name,'Old Woman' AS Label
+FROM parks_and_recreation.employee_demographics
+WHERE age>40 AND gender='Female'
+UNION 
+SELECT first_name,last_name,'Highly Paid Employee' AS Label
+FROM parks_and_recreation.employee_salary
+WHERE salary>70000
+ORDER BY first_name,last_name;
